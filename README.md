1c2521e5990917e8e85b708b5a45f22ba0e74f27
justfid <103446646+justfid@users.noreply.github.com>
2023-11-20 22:53:18 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# To-Do List Application
+
+A python application that keeps track of tasks, which are saved locally on the computer. It has a simplistic and minimalistic UI which is locked to 600 x 400, to not interfere with the rest of the user's running programs.
+
+![Alt text](image.png)
+
+## How It's Made:
+
+**Tech used:** Python, Tkinter
+
+The first thing I did was I imported Tkinter and set up the UI, adding buttons, textboxes, and outputs where neccessary using a "Window" class. Within this class, I also defined some attributes of the window, and I defined some methods which contained the code that gave the buttons functionality. 
+
+One of these methods created a message box whenever the program was shut using the x button, to prompt the user to confirm (and subsequently save their tasks). Within the method to add a task, I ensured there were no duplicates, and also removed any leading or trailing white space, to ensure cleaner inputs. I left the input case sensitive however, as I felt this would impede on the user experience, if I forced it lower case. The deleteTask method uses logic to ensure when the task to delete is chosen, the input is valid, and any exceptions are handled. The final methods of significance, are the save and load task methods, which are called automatically whenever the program is opened, or closed. This method either saves or loads the current task from [Todo.txt](Todo.txt). Overall, this program is simple, yet it is sleek and useful. 
+
+However there are potential future improvements to be made. Most notably, the delete task method is potentially clunky, with the user being prompted to enter a numerical value corresponding to the position of a task. To solve this, the output text box, should be changed to a list box, which is clickable (and the delete method would delete the selected task).
+
+## Optimizations
+
+Most of the optimisations that I implemented were to smoothen the user experience. For example, automatically saving and loading tasks to/from a txt file, as opposed to having to manually save. Furthermore, I added line numbers to the tasks, so the user can easily identify which position the task is located in. Finally, I made the buttons grey in order to differentiate the buttons, but also to keep the UI sleek and minimalistic. Another example of this is the ability to use the enter key instead of the add task button, making the user experience that slight bit more seamless.
+
+## Lessons Learned:
+
+Since this is my first real project, I wanted to not only learn how to create a UI, but also create a practical program with a real world use in my life. I happen to use to do lists very frequently, which inspired me to code one myself. This was a huge learning experience, where not only did I become more comfortable with OOP, but I learned the basics of Tkinter. This project has developed my coding skills with Python, and has also helped to create a new passion and hobby. I hope I can use this newfound knowledge and passion to create bigger and better projects in the future.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ todolist.py
@@ -0,0 +1,144 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+
+class Window():
+    def __init__(self):
+        self.root = tk.Tk()
+
+        #dimensions of screen
+        screenWidth = 600
+        screenHeight = 400
+        #how many pixels from left side is padded
+        xpad = 75
+        defaultFont = ("Calibri", 12)
+
+        self.root.geometry(f"{screenWidth}x{screenHeight}")
+        self.root.title("To-Do List")
+        #ensures screen size cant be changed
+        self.root.resizable(False, False)
+        
+        title = tk.Label(self.root, text = "To-Do List", font = ("Calibri", 18))
+        title.place(x=xpad, y=15)
+
+        subtitle = tk.Label(self.root, text = "Enter Task Title:", font = ("Calibri", 14))
+        subtitle.place(x=xpad, y=100)
+
+        #box to type tasks in
+        self.entry = tk.Entry(self.root, width = 20, font=(14))
+        self.entry.place(x=xpad, y=140)
+
+        #button to add a task - calls custom function
+        add = tk.Button(self.root, text = "Add Task", width = 20, height=1, font = defaultFont, bg = "light grey", command = self.addTask)
+        add.place(x=xpad, y=180)
+
+        #binds enter key to the add task function
+        self.root.bind("<Return>", self.addTask)
+
+        #delete task button
+        delete = tk.Button(self.root, text = "Delete Task", width = 20, height=1, font = defaultFont, bg = "light grey", command = self.deleteTask)
+        delete.place(x=xpad, y=220)
+
+        #delete all tasks button - calls a custom delete all function
+        delete_all = tk.Button(self.root, text = "Delete All Tasks", width = 20, height=1, font = defaultFont, bg = "light grey", command = self.deleteAllTasks)
+        delete_all.place(x=xpad, y=260)
+
+        #exit button - calls custom save text function
+        quit = tk.Button(self.root, text = "Exit", command = self.saveText, width = 20, height = 1, font = defaultFont, bg = "light grey")
+        quit.place(x=xpad, y=300)
+
+        #text box to keep track of index in list (starting from 1)
+        self.indexBox = tk.Text(self.root, height = 14, width=3, font= ("Calibri", 12, "bold"))
+        for counter in range(1,15):
+            self.indexBox.insert("end",f"{counter})\n")
+        self.indexBox.configure(state="disabled")
+        self.indexBox.place(x= 315, y= 100)
+
+        #text box
+        self.outputText = tk.Text(self.root, height=14, width =20, font = defaultFont)
+        #ensures text box cant be changed
+        self.outputText.configure(state="disabled")
+        self.outputText.place(x= 350, y= 100)
+
+        self.loadText()
+        #if x button pressed, causes a save
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.root.mainloop()
+
+
+    def addTask(self, event = None):
+        #adds a task and ensures no duplicated - event parameter needed, idk why
+        self.outputText.configure(state="normal")
+        task = self.entry.get()
+        #strips task to get rid of white space
+        strippedTask = task.lstrip().rstrip()
+        #checks if box is empty
+        if not len(strippedTask):
+            pass
+        #checks if stripped task is in the list already. The \n's stop boundary case errors.
+        elif f"\n{strippedTask}\n" in f"\n{self.outputText.get('1.0', 'end')}":
+            messagebox.showerror("To-Do List", "Task already exists")
+        else:
+            self.entry.delete(0,len(task))
+            self.outputText.insert("end", f"{strippedTask}\n")
+
+        self.outputText.configure(state="disabled")
+
+
+    def deleteTask(self):
+        #deletes a specific task based off its position
+        self.outputText.configure(state="normal")
+        valid = False
+
+        while not valid:
+            userPrompt = "What Task Do You Want To Delete \n(Enter a number that corresponds to the task's position in list)"
+            userInput = simpledialog.askstring(title="Delete", prompt=userPrompt)
+            try:
+                userInput = int(userInput)
+            except ValueError:
+                pass
+            else:
+                valid = True
+
+        self.outputText.delete(f"{userInput}.0", f"{userInput + 1}.0")
+        self.outputText.configure(state="disabled")
+
+
+    def deleteAllTasks(self):
+        #deletes all tasks
+        self.outputText.configure(state="normal")
+        self.outputText.delete("1.0",tk.END)
+        self.outputText.configure(state="disabled")
+
+
+    def saveText(self):
+        #saves the tasks to a txt file, and closes window
+        textFile = open("Todo.txt", "w")
+        textFile.write(self.outputText.get("1.0", "end"))
+        textFile.close()
+        self.root.destroy()
+
+    def loadText(self):
+        #loads text from txt file, and gets rid of a \n
+        textFile = open("Todo.txt", "r")
+        content = textFile.read()
+        self.outputText.configure(state="normal")
+        self.outputText.insert("end",content)
+        self.outputText.delete("end-1c", "end")
+        self.outputText.configure(state="disabled")
+        textFile.close()
+
+    
+    def on_close(self):
+        #function overriding the x button mechanism
+        close = messagebox.askokcancel("Close", "Do you want to quit?")
+        if close:
+            self.saveText()
+    
+def main():
+    Window()
+
+if __name__ == "__main__":
+    main()
+
+    #further improvement later: 
+    #change text box to list box to make tasks clickable to delete
